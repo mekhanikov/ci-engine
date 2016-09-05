4da206d3e165e9f3419c4b3b11367c7bce6899c3
Evgeny Mekhanikov <emekhanikov@thumbtack.net>
2016-09-05 16:31:26 +0600

    Add steps

--- src/main/java/com/ciengine/EnvironmentVariables.java
+++ src/main/java/com/ciengine/EnvironmentVariables.java
@@ -7,6 +7,11 @@ public class EnvironmentVariables
 {
 	public void addProperty(String key, String value)
 	{
+		// TODO
+	}
 
+	public String getProperty(String gitUrl)
+	{
+		return "";// TODO
 	}
 }
