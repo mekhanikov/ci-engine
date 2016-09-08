64af517dade6abbacbb88cfd5ea94799f12ca1f9
Evgeny Mekhanikov <emekhanikov@thumbtack.net>
2016-09-08 18:04:34 +0600

    ssh upload and run

--- master/src/main/resources/build.sh
+++ master/src/main/resources/build.sh
@@ -4,6 +4,8 @@ echo "HELLO MAAAN !!!!"
 ls -lah | grep Dock
 #docker run ubuntu /bin/echo "Hello world!"
 docker build -t ciengine/tst .
- # docker run -i -t tst
-#docker ps
+echo "*** RUN"
+docker run ciengine/tst
+echo "*** DONE"
+docker ps
 
