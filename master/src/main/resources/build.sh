7dc1926989f6e4e2f11323d721a02569e6807c3f
Evgeny Mekhanikov <emekhanikov@thumbtack.net>
2016-09-12 19:02:35 +0600

    ssh upload and run

--- master/src/main/resources/build.sh
+++ master/src/main/resources/build.sh
@@ -4,6 +4,8 @@ echo "HELLO MAAAN !!!!"
 pwd
 ls -lah
 #docker run ubuntu /bin/echo "Hello world!"
+# TODO use volume for mvn repo cache
+# TODO share mvn cache between all docker containers? - no safe in parallel builds?
 docker build -t ciengine/tst .
 echo "*** RUN"
 docker run ciengine/tst
