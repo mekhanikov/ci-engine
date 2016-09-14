91569e9bb93417a1d8b14e9c96806a388405ff99
Evgeny Mekhanikov <emekhanikov@thumbtack.net>
2016-09-14 18:37:28 +0600

    ssh upload and run

---
+++ master/docker/imagetwo/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -x
+echo "HELLO WOMAN !!!!"
+pwd
+ls -lah
+#docker build -t ciengine/imagetwo .
+echo "<<<< RUN"
+#docker run ciengine/imagetwo
+echo "DONE >>>"
+docker ps
+
--- master/src/main/java/com/ciengine/master/BuildRunnerImpl.java
+++ master/src/main/java/com/ciengine/master/BuildRunnerImpl.java
@@ -72,12 +72,12 @@ public class BuildRunnerImpl implements BuildRunner
 			ChannelSftp sftpChannel = (ChannelSftp) session.openChannel("sftp");
 			sftpChannel.connect();
 			System.out.println("SFTP Channel created.");
-			File f1 = new File("D:\\prj\\ci-engine\\master\\src\\main\\resources\\build.sh");
+			File f1 = new File("D:\\prj\\ci-engine\\master\\docker\\" + buildModel.getDockerImageId() + "\\build.sh");
 			//			sftpChannel.rm("tmp");
 			//			sftpChannel.mkdir("tmp");
 			sftpChannel.cd("tmp");
 			sftpChannel.put(new FileInputStream(f1), f1.getName(), ChannelSftp.OVERWRITE);
-			File f2 = new File("D:\\prj\\ci-engine\\master\\docker\\"+buildModel.getDockerImageId()+"\\Dockerfile");
+			File f2 = new File("D:\\prj\\ci-engine\\master\\docker\\" + buildModel.getDockerImageId() + "\\Dockerfile");
 			sftpChannel.put(new FileInputStream(f2), f2.getName(), ChannelSftp.OVERWRITE);
 			//sftpChannel.cd("/home/ev/.ssh");
 			File f3 = new File("C:\\cygwin\\home\\emekhanikov\\.ssh\\id_rsa");
