b6f9638022e579a719ed26cf89c8ba63d3f21db9
Evgeny Mekhanikov <emekhanikov@thumbtack.net>
2016-09-14 15:51:18 +0600

    ssh upload and run

--- master/src/main/java/com/ciengine/master/controllers/getbuilds/GetBuildsController.java
+++ master/src/main/java/com/ciengine/master/controllers/getbuilds/GetBuildsController.java
@@ -1,4 +1,4 @@
-package com.ciengine.master.controllers;
+package com.ciengine.master.controllers.getbuilds;
 
 import com.ciengine.master.dao.BuildDao;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,17 +11,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created by emekhanikov on 06.09.2016.
  */
 @Controller
-public class CIEngineController
+public class GetBuildsController
 {
 	@Autowired
 	private BuildDao buildDao;
 	// TODO 1. Used to receive events from Agents/Slaves. Should be secure?
 	// TODO 2. Used for link from Stash to concrete build: to see logs, artefacts, status and so on.
 	// TODO 3. See list of builds with filtration by fields.
-	@RequestMapping("/")
+	@RequestMapping(value = "/getBuildsResponse",produces = {"application/*"})
 	@ResponseBody
-	String home() {
-		return "Hello World!";
+	GetBuildsResponse getBuildsResponse() {
+		GetBuildsResponse getBuildsResponse = new GetBuildsResponse();
+		return getBuildsResponse;
 	}
 
 }
---
+++ master/src/main/java/com/ciengine/master/controllers/getbuilds/GetBuildsResponse.java
@@ -0,0 +1,16 @@
+package com.ciengine.master.controllers.getbuilds;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+/**
+ * Created by emekhanikov on 14.09.2016.
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class GetBuildsResponse
+{
+private String s = "sdd";
+}
