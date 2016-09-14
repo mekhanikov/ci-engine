76739d1b7dc57e6251c000b9ae8589d68e8d8a4c
Evgeny Mekhanikov <emekhanikov@thumbtack.net>
2016-09-14 16:56:52 +0600

    ssh upload and run

--- master/src/main/java/com/ciengine/master/dao/BuildDao.java
+++ master/src/main/java/com/ciengine/master/dao/BuildDao.java
@@ -2,12 +2,28 @@ package com.ciengine.master.dao;
 
 import com.ciengine.master.model.BuildModel;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
 
 /**
  * Created by emekhanikov on 13.09.2016.
  */
 public class BuildDao extends AbstractDao<BuildModel>
 {
+	public List<BuildModel> getAll()
+	{
+		CriteriaBuilder builder = openSession().getCriteriaBuilder();
 
+		CriteriaQuery<BuildModel> criteria = builder.createQuery( BuildModel.class );
+		Root<BuildModel> root = criteria.from( BuildModel.class );
+		criteria.select( root );
+		//criteria.where( builder.equal( root.get( BuildModel_.name ), "John Doe" ) );
+
+		List<BuildModel> persons = openSession().createQuery( criteria ).getResultList();
+		return persons;
+	}
 
 }
