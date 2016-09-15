6005b9a2f42528e474b651c8400a0d09729451dc
Evgeny Mekhanikov <emekhanikov@thumbtack.net>
2016-09-15 18:34:40 +0600

    ssh upload and run

--- master/docker/imageone/build.sh
+++ master/docker/imageone/build.sh
@@ -6,9 +6,19 @@ ls -lah
 #docker run ubuntu /bin/echo "Hello world!"
 # TODO use volume for mvn repo cache
 # TODO share mvn cache between all docker containers? - no safe in parallel builds?
-docker build -t ciengine/tst .
-echo "*** RUN"
-docker run ciengine/tst
-echo "*** DONE"
+ID=$(uuidgen)
+echo $ID > dockerimageid.txt
+docker build -t $ID .
+
+idc=$(docker create $ID)
+docker cp $idc:/build/agentoutput.log - > agentoutput.log.tar
+docker rm -v $idc
+
+#docker cp $ID:/build/agentoutput.log - > agentoutput.log.tar
+
+#docker build -t ciengine/tst .
+#echo "*** RUN"
+#cciengine/tst
+#echo "*** DONE"
 docker ps
 
