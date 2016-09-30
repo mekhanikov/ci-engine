1d7d7d2842ccf465edf75c11e74ca8d57c738c42
Evgeny Mekhanikov <emekhanikov@thumbtack.net>
2016-09-30 22:25:34 +0600

    fix

--- master/src/test/java/com/ciengine/master/OnCommitEventIntegrationTests.java
+++ master/src/test/java/com/ciengine/master/OnCommitEventIntegrationTests.java
@@ -13,14 +13,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.ArrayList;
 import java.util.List;
