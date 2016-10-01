75dae3a74719fd8244b848c72621026e7f29370f
Evgeny Mekhanikov <emekhanikov@thumbtack.net>
2016-10-01 11:15:51 +0600

    fix

--- master/src/main/java/com/ciengine/master/model/ReleaseModel.java
+++ master/src/main/java/com/ciengine/master/model/ReleaseModel.java
@@ -1,5 +1,7 @@
 package com.ciengine.master.model;
 
+import com.ciengine.common.EnvironmentVariables;
+
 import javax.persistence.*;
 import java.util.Date;
 
@@ -14,245 +16,126 @@ public class ReleaseModel
 	//@GeneratorType()
 	private int id;
 
-	@Column(name = "start_timestamp")
-	//@Temporal(TemporalType.DATE)
-//	@Temporal(TemporalType.TIMESTAMP)
-	private Date startTimestamp;
+	private String moduleNameToRelease;
+	private String mergeFromCommitId; // commit it (e.g. on develop branch) from it merge to release.
+	private String releaseBranchName; // e.g. release/2.0
 
-	@Column(name = "end_timestamp")
-	//@Temporal(TemporalType.DATE)
-	//	@Temporal(TemporalType.TIMESTAMP)
-	private Date endTimestamp;
-
-	private String inputParams; // TODO should be TEXT or so.
-	private String inputParamsHash;
-	private String moduleName;
-	private String branchName;// TODO (merge connected to 2 branches, pass both?)
-	private String executionList;
+	private String goingToRelease; // list of modules. Used by list, if it see module depends on some modu;es from list
+	private String applyList;
 	private String dockerImageId;
-	private String Status;// TODO  (queued, canceled/postponed/not met pre-req, in progress, success, failed)
-	private String nodeId;//Executed on node id (null if Status: queued) - doesn't makes sense
-	private String reasonOfTrigger;
-	private String summary;
-	private String log;
-//			artifacts
+	private String inputParams; // strigified EnvironmentVariables
 
-
-	public int getId()
-	{
+	public int getId() {
 		return id;
 	}
 
-	public void setId(int id)
-	{
+	public void setId(int id) {
 		this.id = id;
 	}
 
-	public Date getStartTimestamp()
-	{
-		return startTimestamp;
+	public String getModuleNameToRelease() {
+		return moduleNameToRelease;
 	}
 
-	public void setStartTimestamp(Date startTimestamp)
-	{
-		this.startTimestamp = startTimestamp;
+	public void setModuleNameToRelease(String moduleNameToRelease) {
+		this.moduleNameToRelease = moduleNameToRelease;
 	}
 
-	public Date getEndTimestamp()
-	{
-		return endTimestamp;
+	public String getMergeFromCommitId() {
+		return mergeFromCommitId;
 	}
 
-	public void setEndTimestamp(Date endTimestamp)
-	{
-		this.endTimestamp = endTimestamp;
+	public void setMergeFromCommitId(String mergeFromCommitId) {
+		this.mergeFromCommitId = mergeFromCommitId;
 	}
 
-	public String getInputParams()
-	{
-		return inputParams;
+	public String getReleaseBranchName() {
+		return releaseBranchName;
 	}
 
-	public void setInputParams(String inputParams)
-	{
-		this.inputParams = inputParams;
+	public void setReleaseBranchName(String releaseBranchName) {
+		this.releaseBranchName = releaseBranchName;
 	}
 
-	public String getInputParamsHash()
-	{
-		return inputParamsHash;
+	public String getGoingToRelease() {
+		return goingToRelease;
 	}
 
-	public void setInputParamsHash(String inputParamsHash)
-	{
-		this.inputParamsHash = inputParamsHash;
+	public void setGoingToRelease(String goingToRelease) {
+		this.goingToRelease = goingToRelease;
 	}
 
-	public String getModuleName()
-	{
-		return moduleName;
+	public String getApplyList() {
+		return applyList;
 	}
 
-	public void setModuleName(String moduleName)
-	{
-		this.moduleName = moduleName;
+	public void setApplyList(String applyList) {
+		this.applyList = applyList;
 	}
 
-	public String getBranchName()
-	{
-		return branchName;
-	}
-
-	public void setBranchName(String branchName)
-	{
-		this.branchName = branchName;
-	}
-
-	public String getExecutionList()
-	{
-		return executionList;
-	}
-
-	public void setExecutionList(String executionList)
-	{
-		this.executionList = executionList;
-	}
-
-	public String getDockerImageId()
-	{
+	public String getDockerImageId() {
 		return dockerImageId;
 	}
 
-	public void setDockerImageId(String dockerImageId)
-	{
+	public void setDockerImageId(String dockerImageId) {
 		this.dockerImageId = dockerImageId;
 	}
 
-	public String getStatus()
-	{
-		return Status;
+	public String getInputParams() {
+		return inputParams;
 	}
 
-	public void setStatus(String status)
-	{
-		Status = status;
+	public void setInputParams(String inputParams) {
+		this.inputParams = inputParams;
 	}
 
-	public String getNodeId()
-	{
-		return nodeId;
-	}
-
-	public void setNodeId(String nodeId)
-	{
-		this.nodeId = nodeId;
-	}
-
-	public String getReasonOfTrigger()
-	{
-		return reasonOfTrigger;
-	}
-
-	public void setReasonOfTrigger(String reasonOfTrigger)
-	{
-		this.reasonOfTrigger = reasonOfTrigger;
-	}
-
-	public String getSummary()
-	{
-		return summary;
-	}
-
-	public void setSummary(String summary)
-	{
-		this.summary = summary;
-	}
-
-	public String getLog()
-	{
-		return log;
-	}
-
-	public void setLog(String log)
-	{
-		this.log = log;
-	}
-
-	@Override public boolean equals(Object o)
-	{
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
 
 		ReleaseModel that = (ReleaseModel) o;
 
-		if (id != that.id)
+		if (id != that.id) return false;
+		if (moduleNameToRelease != null ? !moduleNameToRelease.equals(that.moduleNameToRelease) : that.moduleNameToRelease != null)
 			return false;
-		if (startTimestamp != null ? !startTimestamp.equals(that.startTimestamp) : that.startTimestamp != null)
+		if (mergeFromCommitId != null ? !mergeFromCommitId.equals(that.mergeFromCommitId) : that.mergeFromCommitId != null)
 			return false;
-		if (endTimestamp != null ? !endTimestamp.equals(that.endTimestamp) : that.endTimestamp != null)
+		if (releaseBranchName != null ? !releaseBranchName.equals(that.releaseBranchName) : that.releaseBranchName != null)
 			return false;
-		if (inputParams != null ? !inputParams.equals(that.inputParams) : that.inputParams != null)
+		if (goingToRelease != null ? !goingToRelease.equals(that.goingToRelease) : that.goingToRelease != null)
 			return false;
-		if (inputParamsHash != null ? !inputParamsHash.equals(that.inputParamsHash) : that.inputParamsHash != null)
-			return false;
-		if (moduleName != null ? !moduleName.equals(that.moduleName) : that.moduleName != null)
-			return false;
-		if (branchName != null ? !branchName.equals(that.branchName) : that.branchName != null)
-			return false;
-		if (executionList != null ? !executionList.equals(that.executionList) : that.executionList != null)
-			return false;
+		if (applyList != null ? !applyList.equals(that.applyList) : that.applyList != null) return false;
 		if (dockerImageId != null ? !dockerImageId.equals(that.dockerImageId) : that.dockerImageId != null)
 			return false;
-		if (Status != null ? !Status.equals(that.Status) : that.Status != null)
-			return false;
-		if (nodeId != null ? !nodeId.equals(that.nodeId) : that.nodeId != null)
-			return false;
-		if (reasonOfTrigger != null ? !reasonOfTrigger.equals(that.reasonOfTrigger) : that.reasonOfTrigger != null)
-			return false;
-		if (summary != null ? !summary.equals(that.summary) : that.summary != null)
-			return false;
-		return log != null ? log.equals(that.log) : that.log == null;
+		return inputParams != null ? inputParams.equals(that.inputParams) : that.inputParams == null;
 
 	}
 
-	@Override public int hashCode()
-	{
+	@Override
+	public int hashCode() {
 		int result = id;
-		result = 31 * result + (startTimestamp != null ? startTimestamp.hashCode() : 0);
-		result = 31 * result + (endTimestamp != null ? endTimestamp.hashCode() : 0);
-		result = 31 * result + (inputParams != null ? inputParams.hashCode() : 0);
-		result = 31 * result + (inputParamsHash != null ? inputParamsHash.hashCode() : 0);
-		result = 31 * result + (moduleName != null ? moduleName.hashCode() : 0);
-		result = 31 * result + (branchName != null ? branchName.hashCode() : 0);
-		result = 31 * result + (executionList != null ? executionList.hashCode() : 0);
+		result = 31 * result + (moduleNameToRelease != null ? moduleNameToRelease.hashCode() : 0);
+		result = 31 * result + (mergeFromCommitId != null ? mergeFromCommitId.hashCode() : 0);
+		result = 31 * result + (releaseBranchName != null ? releaseBranchName.hashCode() : 0);
+		result = 31 * result + (goingToRelease != null ? goingToRelease.hashCode() : 0);
+		result = 31 * result + (applyList != null ? applyList.hashCode() : 0);
 		result = 31 * result + (dockerImageId != null ? dockerImageId.hashCode() : 0);
-		result = 31 * result + (Status != null ? Status.hashCode() : 0);
-		result = 31 * result + (nodeId != null ? nodeId.hashCode() : 0);
-		result = 31 * result + (reasonOfTrigger != null ? reasonOfTrigger.hashCode() : 0);
-		result = 31 * result + (summary != null ? summary.hashCode() : 0);
-		result = 31 * result + (log != null ? log.hashCode() : 0);
+		result = 31 * result + (inputParams != null ? inputParams.hashCode() : 0);
 		return result;
 	}
 
-	@Override public String toString()
-	{
-		return "BuildModel{" +
+	@Override
+	public String toString() {
+		return "ReleaseModel{" +
 				"id=" + id +
-				", startTimestamp=" + startTimestamp +
-				", endTimestamp=" + endTimestamp +
-				", inputParams='" + inputParams + '\'' +
-				", inputParamsHash='" + inputParamsHash + '\'' +
-				", moduleName='" + moduleName + '\'' +
-				", branchName='" + branchName + '\'' +
-				", executionList='" + executionList + '\'' +
+				", moduleNameToRelease='" + moduleNameToRelease + '\'' +
+				", mergeFromCommitId='" + mergeFromCommitId + '\'' +
+				", releaseBranchName='" + releaseBranchName + '\'' +
+				", goingToRelease='" + goingToRelease + '\'' +
+				", applyList='" + applyList + '\'' +
 				", dockerImageId='" + dockerImageId + '\'' +
-				", Status='" + Status + '\'' +
-				", nodeId='" + nodeId + '\'' +
-				", reasonOfTrigger='" + reasonOfTrigger + '\'' +
-				", summary='" + summary + '\'' +
-				", log='" + log + '\'' +
+				", inputParams='" + inputParams + '\'' +
 				'}';
 	}
 }
