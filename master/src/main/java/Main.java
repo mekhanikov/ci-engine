7811629e7202c8fb3ae3ca953fea9ce0161155fe
Evgeny Mekhanikov <emekhanikov@thumbtack.net>
2016-12-07 19:47:31 +0600

    add webapp

--- master/src/main/java/Main.java
+++ master/src/main/java/Main.java
@@ -4,6 +4,9 @@
 public class Main
 {
 public static void main(String[] ss) {
+    //http://stackoverflow.com/questions/35488167/how-can-you-find-the-latest-version-of-a-maven-artifact-from-java-using-aether
+    //https://github.com/eclipse/aether-demo/tree/master/aether-demo-snippets/src/main/java/org/eclipse/aether/examples
+
     //http://blogs.atlassian.com/2014/05/handle-big-repositories-git/
     // Do in cronjob
     //  for each module if not cloned - clone
