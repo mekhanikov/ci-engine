b24e6e459c4f3438131dcd50cd5da1bc3b38db6b
Evgeny Mekhanikov <emekhanikov@thumbtack.net>
2016-12-07 19:39:09 +0600

    add webapp

--- master/src/main/java/Main.java
+++ master/src/main/java/Main.java
@@ -3,5 +3,11 @@
  */
 public class Main
 {
-
+public static void main(String[] ss) {
+    //http://blogs.atlassian.com/2014/05/handle-big-repositories-git/
+    // Do in cronjob
+    //  for each module if not cloned - clone
+    //  pull
+   // git rev-list --format=oneline  --left-right release/6.1.0...release/6.3.0
+}
 }
