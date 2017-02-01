3610d7cdb565b246cfd256c9df67d7a7618f6f28
Evgeny Mekhanikov <emekhanikov@thumbtack.net>
2017-02-01 13:50:13 +0600

    add sourcesrepository

--- agent/src/main/java/com/ciengine/agent/lists/current/MockReleaseList.java
+++ agent/src/main/java/com/ciengine/agent/lists/current/MockReleaseList.java
@@ -1,116 +1,37 @@
 package com.ciengine.agent.lists.current;
 
 
-import com.ciengine.common.*;
-import com.ciengine.common.dto.AddBuildRequest;
-import com.ciengine.common.dto.AddBuildResponse;
-import com.ciengine.common.dto.Build;
-import com.ciengine.common.events.OnNewArtifactEvent;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.ciengine.common.EnvironmentVariables;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 
 /**
  * Created by emekhanikov on 05.09.2016.
  */
 @Component
-public class MockReleaseList implements CIEngineList
-{// TODO rename to ReleaseList
-	@Autowired
-	private CIEngineClient ciEngineClient;
-
-	@Autowired
-	@Qualifier("mavenStep")
-	private CIEngineStep ciEngineStep;
-
-	@Override public void doList(EnvironmentVariables environmentVariables) throws CIEngineStepException
-	{
-		String gitUrl = environmentVariables.getProperty(EnvironmentVariablesConstants.GIT_URL);
-		String branchName = environmentVariables.getProperty(EnvironmentVariablesConstants.RELEASE_BRANCH_NAME);
-		String commitId = environmentVariables.getProperty(EnvironmentVariablesConstants.COMMIT_ID);
-		String buildId = environmentVariables.getProperty(EnvironmentVariablesConstants.BUILD_EXTERNAL_ID);
-		String goingToRelease = environmentVariables.getProperty(EnvironmentVariablesConstants.GOING_TO_RELEASE);
-		String moduleNameToRelease = environmentVariables.getProperty(EnvironmentVariablesConstants.MODULE_NAME);
-		String url = environmentVariables.getProperty(EnvironmentVariablesConstants.CIENGINE_MASTER_URL);
-		String dockerImageId = environmentVariables.getProperty(EnvironmentVariablesConstants.DOCKER_IMAGE_ID);
-
-		//String moduleName = moduleNameToRelease.split(":")[0];
-
-		AddBuildRequest addBuildRequest = new AddBuildRequest();
-		addBuildRequest.setExecutionList("mockReleaseList");
-		addBuildRequest.setDockerImageId(dockerImageId);
-		addBuildRequest.setModuleName(moduleNameToRelease);
-		addBuildRequest.setBranchName(branchName);
-		AddBuildResponse addBuildResponse = ciEngineClient.findBuild(url, addBuildRequest);
-        // TODO calch hash and gilter by hash as well.
-        // TODO except current one build - it alwasy IN PROGRESS
-		List<Build> buildLists = addBuildResponse.getBuildList();
-
-        // If has at least one build IN PROGRESS/QUEUED/FAILED (check hash as well, it should be the same?) - skip current one - no need build the same.
-
-        boolean allAreSkipped = true;
-        if (buildLists != null) {
-            for (Build build : buildLists) {
-                if ( !build.getExternalId().equals(buildId) && !BuildStatus.SKIPED.equals(build.getStatus())) {
-                    allAreSkipped = false;
-                }
-            }
-        }
-
-		// If build (with the latest startTimestamp?) is skipped - rebuild
-		//String lastBuildStatus = buildLists != null && buildLists.size() > 0 ? buildLists.get(0).getStatus() : null;
-		if (allAreSkipped) {
-			if (!ciEngineClient.isModuleReleased(url, moduleNameToRelease)) {// TODO how it was released if all builds are skipped?
-				Set<String> waitingModules = allDepsInPlace(url, moduleNameToRelease, goingToRelease);
-				if (waitingModules.isEmpty()) {
-					System.out.print("d");
-					try {
-						Thread.sleep(5000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					//throw new CIEngineStepException("");
-					// TODO parse Maven logs for "Uploaded artefacts?"
-					// TODO are we interesting in Artefacts released? or in Module released? For Module Released we can not scan logs then.
-					ciEngineStep.doStep(environmentVariables);
-					OnNewArtifactEvent onNewArtifactEvent = new OnNewArtifactEvent();
-					onNewArtifactEvent.setComitId(commitId);
-					onNewArtifactEvent.setGitUrl(gitUrl);
-					onNewArtifactEvent.setBranchName(branchName);
-					onNewArtifactEvent.setModuleName(moduleNameToRelease);
-					ciEngineClient.setBuildStatus(url, buildId, BuildStatus.SUCCESS, "RELEASED");
-					ciEngineClient.sendEvent(url, onNewArtifactEvent);
-				} else {
-					System.out.print("DEPS ARE REQUIRED");
-					ciEngineClient.setBuildStatus(url, buildId, BuildStatus.SKIPED, "DEPS ARE REQUIRED: " + String.join(", ", waitingModules));
-				}
-
-				// TODO read deps from pom.xml
-				// Map each dep artefact to module (each module can be related to multiple artefacts).
-				// Check if the module in list of going to release.
-				// If in list, check if isModuleReleased
-				// if not all required modules is released, SKIPPED, reason = ModA:1, ModB:2 are required, but has not been released yet
-			} else {
-                // TODO never will be here (how it was released if all builds are skipped?)?! but we are here
-				System.out.print("ALREADY RELEASED");
-				// TODO SKIPED, reason = already released.
-				// delete Release from DB (need releaseId in in args)
-				ciEngineClient.setBuildStatus(url, buildId, BuildStatus.SKIPED, "ALREADY RELEASED");
-			}
-		} else {
-			System.out.println("NOT ALLARESKIPPED");
-			if (ciEngineClient.isModuleReleased(url, moduleNameToRelease)) {
-				ciEngineClient.setBuildStatus(url, buildId, BuildStatus.SKIPED, "ALREADY RELEASED");
-			} else {
-				ciEngineClient.setBuildStatus(url, buildId, BuildStatus.SKIPED, "NOT ALLARESKIPPED");
-			}
-        }
+public class MockReleaseList extends ReleaseList
+{
+	protected String getExecutionListName() {
+		return "mockReleaseList";
 	}
 
-	private Set<String> allDepsInPlace(String url, String module0, String goingToRelease) {
+	protected void build(EnvironmentVariables environmentVariables) {
+		//throw new CIEngineStepException("");
+		// TODO parse Maven logs for "Uploaded artefacts?"
+		// TODO are we interesting in Artefacts released? or in Module released? For Module Released we can not scan logs then.
+		System.out.print("d");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected Set<String> allDepsInPlace(String url, String module0, String goingToRelease) {
 		Set<String> goingToReleaseModules = new HashSet<>(Arrays.asList(goingToRelease.split(",")));
 		String moduleName = module0.split(":")[0];
 		String moduleVersion = module0.split(":")[1];
@@ -125,7 +46,7 @@ public class MockReleaseList implements CIEngineList
 
 		Set<String> waitingModules = new HashSet<>();
 		for (String requiredModule : requiredModules) {
-			if (goingToReleaseModules.contains(requiredModule) && !ciEngineClient.isModuleReleased(url, requiredModule)) {
+			if (goingToReleaseModules.contains(requiredModule) && !getCiEngineClient().isModuleReleased(url, requiredModule)) {
 				waitingModules.add(requiredModule);
 			}
 		}
---
+++ agent/src/main/java/com/ciengine/agent/lists/current/ReleaseList.java
@@ -0,0 +1,131 @@
+package com.ciengine.agent.lists.current;
+
+
+import com.ciengine.common.*;
+import com.ciengine.common.dto.AddBuildRequest;
+import com.ciengine.common.dto.AddBuildResponse;
+import com.ciengine.common.dto.Build;
+import com.ciengine.common.events.OnNewArtifactEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+/**
+ * Created by emekhanikov on 05.09.2016.
+ */
+@Component
+public abstract class ReleaseList implements CIEngineList
+{// TODO rename to ReleaseList
+	@Autowired
+	private CIEngineClient ciEngineClient;
+
+	@Autowired
+	@Qualifier("mavenStep")
+	private CIEngineStep ciEngineStep;
+
+	@Override public void doList(EnvironmentVariables environmentVariables) throws CIEngineStepException
+	{
+		String gitUrl = environmentVariables.getProperty(EnvironmentVariablesConstants.GIT_URL);
+		String branchName = environmentVariables.getProperty(EnvironmentVariablesConstants.RELEASE_BRANCH_NAME);
+		String commitId = environmentVariables.getProperty(EnvironmentVariablesConstants.COMMIT_ID);
+		String buildId = environmentVariables.getProperty(EnvironmentVariablesConstants.BUILD_EXTERNAL_ID);
+		String goingToRelease = environmentVariables.getProperty(EnvironmentVariablesConstants.GOING_TO_RELEASE);
+		String moduleNameToRelease = environmentVariables.getProperty(EnvironmentVariablesConstants.MODULE_NAME);
+		String url = environmentVariables.getProperty(EnvironmentVariablesConstants.CIENGINE_MASTER_URL);
+		String dockerImageId = environmentVariables.getProperty(EnvironmentVariablesConstants.DOCKER_IMAGE_ID);
+
+		//String moduleName = moduleNameToRelease.split(":")[0];
+
+		AddBuildRequest addBuildRequest = new AddBuildRequest();
+		addBuildRequest.setExecutionList(getExecutionListName());
+		addBuildRequest.setDockerImageId(dockerImageId);
+		addBuildRequest.setModuleName(moduleNameToRelease);
+		addBuildRequest.setBranchName(branchName);
+		AddBuildResponse addBuildResponse = ciEngineClient.findBuild(url, addBuildRequest);
+        // TODO calch hash and gilter by hash as well.
+        // TODO except current one build - it alwasy IN PROGRESS
+		List<Build> buildLists = addBuildResponse.getBuildList();
+
+        // If has at least one build IN PROGRESS/QUEUED/FAILED (check hash as well, it should be the same?) - skip current one - no need build the same.
+
+        boolean allAreSkipped = true;
+        if (buildLists != null) {
+            for (Build build : buildLists) {
+                if ( !build.getExternalId().equals(buildId) && !BuildStatus.SKIPED.equals(build.getStatus())) {
+                    allAreSkipped = false;
+                }
+            }
+        }
+
+		// If build (with the latest startTimestamp?) is skipped - rebuild
+		//String lastBuildStatus = buildLists != null && buildLists.size() > 0 ? buildLists.get(0).getStatus() : null;
+		if (allAreSkipped) {
+			if (!ciEngineClient.isModuleReleased(url, moduleNameToRelease)) {// TODO how it was released if all builds are skipped?
+				Set<String> waitingModules = allDepsInPlace(url, moduleNameToRelease, goingToRelease);
+				if (waitingModules.isEmpty()) {
+					build(environmentVariables);
+
+					ciEngineStep.doStep(environmentVariables);
+					OnNewArtifactEvent onNewArtifactEvent = new OnNewArtifactEvent();
+					onNewArtifactEvent.setComitId(commitId);
+					onNewArtifactEvent.setGitUrl(gitUrl);
+					onNewArtifactEvent.setBranchName(branchName);
+					onNewArtifactEvent.setModuleName(moduleNameToRelease);
+					ciEngineClient.setBuildStatus(url, buildId, BuildStatus.SUCCESS, "RELEASED");
+					ciEngineClient.sendEvent(url, onNewArtifactEvent);
+				} else {
+					System.out.print("DEPS ARE REQUIRED");
+					ciEngineClient.setBuildStatus(url, buildId, BuildStatus.SKIPED, "DEPS ARE REQUIRED: " + String.join(", ", waitingModules));
+				}
+
+				// TODO read deps from pom.xml
+				// Map each dep artefact to module (each module can be related to multiple artefacts).
+				// Check if the module in list of going to release.
+				// If in list, check if isModuleReleased
+				// if not all required modules is released, SKIPPED, reason = ModA:1, ModB:2 are required, but has not been released yet
+			} else {
+                // TODO never will be here (how it was released if all builds are skipped?)?! but we are here
+				System.out.print("ALREADY RELEASED");
+				// TODO SKIPED, reason = already released.
+				// delete Release from DB (need releaseId in in args)
+				ciEngineClient.setBuildStatus(url, buildId, BuildStatus.SKIPED, "ALREADY RELEASED");
+			}
+		} else {
+			System.out.println("NOT ALLARESKIPPED");
+			if (ciEngineClient.isModuleReleased(url, moduleNameToRelease)) {
+				ciEngineClient.setBuildStatus(url, buildId, BuildStatus.SKIPED, "ALREADY RELEASED");
+			} else {
+				ciEngineClient.setBuildStatus(url, buildId, BuildStatus.SKIPED, "NOT ALLARESKIPPED");
+			}
+        }
+	}
+
+	protected abstract void build(EnvironmentVariables environmentVariables);
+
+	protected abstract String getExecutionListName();
+
+	protected abstract Set<String> allDepsInPlace(String url, String module0, String goingToRelease);
+
+
+	public CIEngineClient getCiEngineClient() {
+		return ciEngineClient;
+	}
+
+	public void setCiEngineClient(CIEngineClient ciEngineClient) {
+		this.ciEngineClient = ciEngineClient;
+	}
+
+	public CIEngineStep getCiEngineStep() {
+		return ciEngineStep;
+	}
+
+	public void setCiEngineStep(CIEngineStep ciEngineStep) {
+		this.ciEngineStep = ciEngineStep;
+	}
+}
