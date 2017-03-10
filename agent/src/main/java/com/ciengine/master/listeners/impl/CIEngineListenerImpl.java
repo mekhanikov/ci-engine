6882790ef480eb7c42297ff79eabe9ee438d1597
Evgeny <emekhanikov@gmail.com>
2017-03-10 23:17:16 +0600

    fix

--- agent/src/main/java/com/ciengine/master/listeners/impl/CIEngineListenerImpl.java
+++ agent/src/main/java/com/ciengine/master/listeners/impl/CIEngineListenerImpl.java
@@ -1,52 +1,14 @@
 package com.ciengine.master.listeners.impl;
 
-
-import com.ciengine.common.CIEngineEvent;
-import com.ciengine.common.DefaultCIEngineEvent;
-import com.ciengine.master.listeners.CIEngineListener;
-import com.ciengine.master.listeners.CIEngineListenerBuilder;
-import com.ciengine.master.listeners.CIEngineListenerException;
-import com.ciengine.master.listeners.RuleBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
-
-
 /**
- * Created by emekhanikov on 05.09.2016.
+ * Created by evgenymekhanikov on 10.03.17.
  */
 @Component(value = "CIEngineListener")
-
-public class CIEngineListenerImpl implements CIEngineListener
-{
-	private List<RuleBuilder> ruleBuilderList = new ArrayList<>();
-	private List<CIEngineListener> ciEngineListenerList = new ArrayList<>();
-
-	@Autowired
-	private ApplicationContext applicationContext;
-
-	@PostConstruct
-	public void init() {
-		createRules();
-		ruleBuilderList.forEach(t->{ciEngineListenerList.add(t.getCIEngineListenerBuilder().createCIEngineListener());});
-	}
-
-	@Override
-	public void onEvent(CIEngineEvent ciEngineEvent) throws CIEngineListenerException {
-		ciEngineListenerList.forEach(t->{
-			try {
-				t.onEvent(ciEngineEvent);
-			} catch (CIEngineListenerException e) {
-				e.printStackTrace();
-			}
-		});
-	}
-
-	protected void createRules() {
+public class CIEngineListenerImpl extends AbstractCIEngineListenerImpl {
+    @Override
+    protected void createRules() {
 		//createRuleBuilder(ciEngineEvent).onCommit().forModules("modA").forBranches("develop, feature/.*").applyList("onCommitList").triggerBuild();
 		//createRuleBuilder().onCommit().forModules("modA").forBranches("develop").triggerBuildsFor("modA", "feature/.*");
 		//		createRuleBuilder().onCommit().forModules("modA").
@@ -55,16 +17,5 @@ public class CIEngineListenerImpl implements CIEngineListener
 		createRuleBuilder().onNewArtefact().processReleaseRule();
 		createRuleBuilder().onReleaseSubmited().triggerRelease();
 		createRuleBuilder().onCommit().forModules("modA").forBranches("develop").triggerBuild();
-	}
-
-	protected RuleBuilder createRuleBuilder() {
-		RuleBuilder ruleBuilder = applicationContext.getBean(RuleBuilder.class);
-		ruleBuilderList.add(ruleBuilder);
-		return ruleBuilder;
-	}
-
-	@Override
-	public boolean isEventApplicable(CIEngineEvent defaultCIEngineEvent) {
-		return true;
-	}
+    }
 }
