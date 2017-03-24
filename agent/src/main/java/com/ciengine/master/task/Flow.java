e1e1b069c325e2f2de6259947014ddf6ca4c57f2
Evgeny Mekhanikov <emekhanikov@thumbtack.net>
2017-03-24 18:40:07 +0600

    add tasks

---
+++ agent/src/main/java/com/ciengine/master/task/Flow.java
@@ -0,0 +1,13 @@
+package com.ciengine.master.task;
+
+/**
+ * Created by emekhanikov on 24.03.2017.
+ */
+public abstract class Flow {
+    private String name;
+    public Flow(String name) {
+this.name = name;
+    }
+
+    abstract void createFlow();
+}
--- agent/src/main/java/com/ciengine/master/task/TaskPipelineImpl.java
+++ agent/src/main/java/com/ciengine/master/task/TaskPipelineImpl.java
@@ -16,6 +16,30 @@ public class TaskPipelineImpl extends AbstractPipelineImpl {
 
     @Override
     protected void prepareAll() {
+        Flow flow = new Flow("build CS") {
+            @Override
+            void createFlow() {// TODO add FlowContext? to pas arguments from event/action?
+                Task createBinaries = new Task("createBinaries");
+                Task createSources = new Task("createSources");
+
+                List<Task> tests = new ArrayList<>();
+                for(int i=0; i < 2; i++) {
+                    Task task = new Task("test" +i);
+                    task.dependsOn(createBinaries, createSources);
+                    tests.add(task);
+                }
+
+                Task javadocTask = new Task("javadocTask");
+                javadocTask.dependsOn(createBinaries, createSources);
+
+                Task deployTask = new Task("deployTask");
+
+                Task[] myArray = tests.toArray(new Task[0]);
+                deployTask.dependsOn(createBinaries, createSources, javadocTask);
+                deployTask.dependsOn(myArray);
+            }
+        };
+        // TODO on commit to pom.xml or on manual trigger (sction?) run flow.
         //
 //        createRuleBuilder().onCommit().forModules("modA").forBranches("develop").triggerBuild();
     }
